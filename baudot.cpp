51dbe98489ffdaa37568e3229628d6744a7165b3
Klaus Roemer <kr@prds.rocks>
2017-05-31 13:10:00 +0200

    erste Version

---
+++ baudot.cpp
@@ -0,0 +1,64 @@
+#include "baudot.h"
+
+
+byte baudot_recieving_mode=BAUDOT_MODE_UNDEFINED;
+byte baudot_sending_mode=BAUDOT_MODE_UNDEFINED;
+
+
+char baudotToAscii(byte baudot){
+    baudot_sending_mode=BAUDOT_MODE_UNDEFINED;
+
+  if (baudot == BAUDOT_MODE_ZI){
+    baudot_recieving_mode=BAUDOT_MODE_ZI;
+//    Serial.print("ZI \n");
+  }else if (baudot == BAUDOT_MODE_BU){
+    baudot_recieving_mode=BAUDOT_MODE_BU;
+//    Serial.print("BU \n");
+  }
+
+ if (baudot_recieving_mode==BAUDOT_MODE_BU){
+      return baudot_bu[baudot];
+ }
+ if (baudot_recieving_mode==BAUDOT_MODE_ZI){
+      return baudot_zi[baudot];
+ }
+ return BAUDOT_CHAR_UNKNOWN;
+}
+byte asciiToBaudot(char ascii_in,byte *modeswtichcode){
+  baudot_recieving_mode=BAUDOT_MODE_UNDEFINED;
+  *modeswtichcode=BAUDOT_MODE_UNDEFINED;
+  char ascii=tolower(ascii_in);
+
+  if(baudot_sending_mode==BAUDOT_MODE_ZI){
+    for (byte i = 0 ; i < 32 ; i++){
+      if (ascii == baudot_zi[i]){
+        return i;
+      }
+    }
+    for (byte i = 0 ; i < 32 ; i++){
+      if (ascii == baudot_bu[i]){
+        *modeswtichcode=BAUDOT_MODE_BU;
+        baudot_sending_mode=BAUDOT_MODE_BU;
+        return i;
+      }
+    }
+  }//baudot_sending_mode==BAUDOT_MODE_ZI
+
+  for (byte i = 0 ; i < 32 ; i++){
+    if (ascii == baudot_bu[i]){
+      if(baudot_sending_mode!=BAUDOT_MODE_BU){ // maybe still undefined
+        *modeswtichcode=BAUDOT_MODE_BU;
+        baudot_sending_mode=BAUDOT_MODE_BU;
+      }
+      return i;
+    }
+  }
+  for (byte i = 0 ; i < 32 ; i++){
+    if (ascii == baudot_zi[i]){
+      *modeswtichcode=BAUDOT_MODE_ZI;
+       baudot_sending_mode=BAUDOT_MODE_ZI;
+      return i;
+    }
+  }
+  return BAUDOT_CHAR_UNKNOWN;
+}
---
+++ baudot.h
@@ -0,0 +1,26 @@
+#ifndef __BAUDOT_H__
+#define __BAUDOT_H__
+#include <Arduino.h>
+
+#define itelex
+
+#define BAUDOT_MODE_UNDEFINED 0
+#define BAUDOT_MODE_BU 31
+#define BAUDOT_MODE_ZI 27
+
+
+#ifdef itelex
+ const char baudot_bu[] = "#t\ro hnm\nlrgipcvezdbsyfxawj\017uqk\016";
+ const char baudot_zi[] = "#5\r9 #,.\n)4#80:=3+@?'6#/-2%\01771(\016";
+ #define BAUDOT_CHAR_UNKNOWN 0
+#else
+ const char baudot_bu[] = "\000t\ro hnm\nlrgipcvezdbsyfxawj\017uqk\016";
+ const char baudot_zi[] = "\0005\r9 #,.\n)4#80:=3+\005?'6#/-2\007\01771(\016";
+ #define BAUDOT_CHAR_UNKNOWN 26
+#endif
+
+
+extern char baudotToAscii(byte baudot);
+extern byte asciiToBaudot(char ascii,byte *modeswtichcode);
+
+#endif //ndef __BAUDOT_H__
